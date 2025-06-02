dec56a6821de0a0afe4ce0ddd3e23bbaa0c292f9
ephemeraDream <w568674236@gmail.com>
2025-06-02 17:56:15 +0800

    change

---
+++ assets/bundle-simple.js
@@ -0,0 +1,6 @@
+const product_items = document.querySelectorAll(".bundle_simple .bundle_simple_product")
+
+product_items.forEach(item => {
+  const img_contain = item.querySelector(".bundle_simple_product_img img")
+  const price_contain = item.querySelector(".bundle_simple_product_price")
+})
