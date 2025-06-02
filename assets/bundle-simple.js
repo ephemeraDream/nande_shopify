3522692b3368863fbd208a4603f71333f1c86f38
ephemeraDream <w568674236@gmail.com>
2025-06-02 18:17:47 +0800

    change

--- assets/bundle-simple.js
+++ assets/bundle-simple.js
@@ -1,6 +1,10 @@
 const product_items = document.querySelectorAll(".bundle_simple .bundle_simple_product")
+const bundle_simple_data = JSON.parse(document.getElementById('bundle_simple_data').textContent);
 
 product_items.forEach(item => {
+  const product = bundle_simple_data[item.getAttribute("data-id")].product
+  const variant = bundle_simple_data[item.getAttribute("data-id")].variant
+  console.log(product, variant)
   const img_contain = item.querySelector(".bundle_simple_product_img img")
   const price_contain = item.querySelector(".bundle_simple_product_price")
 })
