9bc4f174d7d62496e4fb73d40735f8729bd4376b
ephemeraDream <w568674236@gmail.com>
2025-06-21 16:30:52 +0800

    change

--- assets/collection-compare.js
+++ assets/collection-compare.js
@@ -9,3 +9,9 @@ show_filter.addEventListener("click", () => {
     filter_contain.style.display = "flex"
   }
 })
+
+const select_switch = document.querySelector(".collection_compare_contain_select_switch_box")
+select_switch.addEventListener("click", () => {
+  select_switch.querySelector(".collection_compare_contain_select_switch").classList.toggle("active")
+  document.querySelector(".collection_compare_contain_body_right").classList.toggle("show_select")
+})
