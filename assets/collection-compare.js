38ac99aebb7380e567844cf7a8a8e288f4594ed7
ephemeraDream <w568674236@gmail.com>
2025-06-21 16:51:38 +0800

    change

--- assets/collection-compare.js
+++ assets/collection-compare.js
@@ -19,11 +19,14 @@ select_switch.addEventListener("click", () => {
 const show_types = document.querySelectorAll(".collection_compare_contain_showtype")
 show_types.forEach(item => {
   item.addEventListener("click", () => {
+    if (item.classList.contains("active")) return
     show_types.forEach(item => item.classList.remove("active"))
     item.classList.add("active")
     const type = item.getAttribute("data-type")
     if (type === "list") {
-      document.querySelector(".collection_compare_contain_body_right").classList.toggle("collection_compare_contain_body_right_list")
+      document.querySelector(".collection_compare_contain_body_right").classList.add("collection_compare_contain_body_right_list")
+    } else {
+      document.querySelector(".collection_compare_contain_body_right").classList.remove("collection_compare_contain_body_right_list")
     }
   })
 })
