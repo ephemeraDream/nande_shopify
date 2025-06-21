066b7b55354ff0e83b769a7d9d28ffe1787ec67d
ephemeraDream <w568674236@gmail.com>
2025-06-21 22:48:29 +0800

    change

--- assets/collection-compare.js
+++ assets/collection-compare.js
@@ -1,3 +1,4 @@
+//侧拉筛选
 const show_filter = document.querySelector(".collection_compare_contain_switchfilter")
 show_filter.addEventListener("click", () => {
   const filter_contain = document.querySelector(".collection_compare_contain_body_left")
@@ -9,14 +10,7 @@ show_filter.addEventListener("click", () => {
     filter_contain.style.display = "flex"
   }
 })
-
-const select_switch = document.querySelector(".collection_compare_contain_select_switch_box")
-select_switch.addEventListener("click", () => {
-  select_switch.querySelector(".collection_compare_contain_select_switch").classList.toggle("active")
-  document.querySelector(".collection_compare_contain_body_right").classList.toggle("show_select")
-  document.querySelector(".collection_compare_select_btn").classList.toggle("show")
-})
-
+//展示方式
 const show_types = document.querySelectorAll(".collection_compare_contain_showtype")
 show_types.forEach(item => {
   item.addEventListener("click", () => {
@@ -31,9 +25,16 @@ show_types.forEach(item => {
     }
   })
 })
-
+//对比选择
+const select_switch = document.querySelector(".collection_compare_contain_select_switch_box")
+const collection_compare_select_btn = document.querySelector(".collection_compare_select_btn")
 const collection_compare_select = document.querySelector(".collection_compare_select")
-document.querySelector(".collection_compare_select_btn").addEventListener("click", () => {
+select_switch.addEventListener("click", () => {
+  select_switch.querySelector(".collection_compare_contain_select_switch").classList.toggle("active")
+  document.querySelector(".collection_compare_contain_body_right").classList.toggle("show_select")
+  collection_compare_select_btn.classList.toggle("show")
+})
+collection_compare_select_btn.addEventListener("click", () => {
   collection_compare_select.setAttribute("open", true)
   document.body.style.overflowY = "hidden";
 })
@@ -47,3 +48,10 @@ document.querySelector(".collection_compare_select_contain_body_list_tip_close")
   collection_compare_select.removeAttribute("open")
   document.body.style.overflowY = "auto";
 });
+document.querySelectorAll(".collection_compare_product_select").forEach(item => {
+  item.addEventListener("click", (e) => {
+    e.stopPropagation()
+    const parent = e.target.closest(".collection_compare_product")
+    parent.classList.toggle("selected")
+  })
+})
