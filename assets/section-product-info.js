3f4372a6f0951eb16e49434eaa27866513054b3f
ephemeraDream <w568674236@gmail.com>
2025-06-29 15:31:22 +0800

    change

---
+++ assets/section-product-info.js
@@ -0,0 +1,29 @@
+// swiper相关
+let imgthumbSwiper, imgboxSwiper;
+let initialSlide = 0;
+function initSwiper() {
+  imgthumbSwiper = new Swiper(".imgthumb_swiper", {
+    loop: true,
+    spaceBetween: 8,
+    slidesPerView: 4,
+    navigation: {
+      nextEl: ".imgthumb_swiper .imgthumb_swiper_next",
+      prevEl: ".imgthumb_swiper .imgthumb_swiper_prev",
+    },
+  });
+  imgboxSwiper = new Swiper(".imgmain_swiper", {
+    loop: true,
+    effect: "fade",
+    fadeEffect: {
+      crossFade: true,
+    },
+    spaceBetween: 0,
+    navigation: {
+      nextEl: ".imgmain_swiper .imgmain_swiper_next",
+      prevEl: ".imgmain_swiper .imgmain_swiper_prev",
+    },
+    thumbs: {
+      swiper: imgthumbSwiper,
+    },
+  });
+}
