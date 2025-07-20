ab16a6524b6dbf3384583782e19e0fdfa91ff91f
ephemeraDream <w568674236@gmail.com>
2025-07-20 15:43:53 +0800

    change

--- assets/cart-drawer.js
+++ assets/cart-drawer.js
@@ -168,8 +168,8 @@ class CartDrawerItems extends CartItems {
 
         observer.unobserve(productRecommendationsSection);
 
-        const url = productRecommendationsSection.dataset.url;
-        // const url = `recommendations/products.json?product_id=${productRecommendationsSection.dataset.id}`
+        // const url = productRecommendationsSection.dataset.url;
+        const url = `recommendations/products.json?product_id=${productRecommendationsSection.dataset.id}&intent=complementary`
 
         fetch(url)
           .then((response) => response.text())
