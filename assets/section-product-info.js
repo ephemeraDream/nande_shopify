aa3b166abce4f6e4686e588eff36f8b80a007ff0
ephemeraDream <w568674236@gmail.com>
2025-07-27 14:33:50 +0800

    change

--- assets/section-product-info.js
+++ assets/section-product-info.js
@@ -325,7 +325,7 @@ function initBuybox() {
         const bundle_tabletop = document.querySelector("input[name='bundle_tabletop_id']")
         if (bundle_tabletop) {
           cartFormData.items.push({
-            id: bundle_tabletop.id, quantity: Number(quantityInput.value), properties: {
+            id: bundle_tabletop.value, quantity: Number(quantityInput.value), properties: {
               'bundle_parent_id': goods_id
             }
           });
