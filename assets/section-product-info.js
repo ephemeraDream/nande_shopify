a82c2f02ea57f08882fee2b9e4a892a2dd1cd3a7
ephemeraDream <w568674236@gmail.com>
2025-08-31 13:49:41 +0800

    change

--- assets/section-product-info.js
+++ assets/section-product-info.js
@@ -697,15 +697,15 @@ function updateImagesByVariantMedia() {
     }
 
     // 主图作为可见序列的第 0 张
-    const activeIndex = 0;
+    const activeIndex = featIdx;
     return { activeIndex };
   };
 
   const { activeIndex } = applyTo('.imgmain_swiper .swiper-slide');
   applyTo('.imgthumb_swiper .swiper-slide');
 
-  imgthumbSwiper.update();
-  imgboxSwiper.update();
+  // imgthumbSwiper.update();
+  // imgboxSwiper.update();
   imgboxSwiper.slideTo(activeIndex);
   imgthumbSwiper.slideTo(activeIndex);
   document.querySelector(".product_info_left_thumb_select_item[data-type='image']").click()
